b274f99803850df5283d2223b117d9235f5358f7
BrunoConti <bruno.conti1997@gmail.com>
2020-12-05 13:10:57 -0300

    Coneccion a mongodb atlas

--- src/index.js
+++ src/index.js
@@ -1,11 +1,37 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const routesV1 = require('./routes/v1');
 
 app = express();
 
+console.log('env', process.env.MONGO_DB);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
 routesV1(app);
 
-app.listen(4000, () => {
-  console.log('Server running on 4000');
-});
+const PORT = process.env.PORT || 4000;
+
+mongoose
+  .connect(process.env.MONGO_DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log('Connected to MONGO DB');
+    app.listen(PORT, () => {
+      console.log(`Server running on ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log('Mongo error', err);
+  });
