419c309e9f3e8bab4430062f60014417de622c76
BrunoConti <bruno.conti1997@gmail.com>
2020-12-05 15:41:08 -0300

    Agrego metodos get, create a products controller

--- src/controllers/v1/productsController.js
+++ src/controllers/v1/productsController.js
@@ -1,7 +1,51 @@
-const createProduct = (req, res) => {};
+const Product = require('../../models/products');
+
+const createProduct = async (req, res) => {
+  try {
+    const { title, description, price, userId } = req.body;
+
+    const product = await Product.create({
+      title,
+      description,
+      price,
+      user: userId,
+    });
+
+    res.send({ status: 'OK', data: product });
+  } catch (err) {
+    res.status(400).send({ status: 'Error', message: err.message });
+  }
+};
 
 const deleteProduct = (req, res) => {};
 
-const getProducts = (req, res) => {};
+const getProducts = async (req, res) => {
+  try {
+    const product = await Product.find()
+      .select('title description price')
+      .populate('user', 'id username email');
 
-module.exports = { createProduct, deleteProduct, getProducts };
+    res.send({ status: 'OK', data: product });
+  } catch (err) {
+    res.status(400).send({ status: 'Error', message: err.message });
+  }
+};
+
+const getProductsByUser = async (req, res) => {
+  try {
+    const product = await Product.find({
+      user: req.params.userId,
+    });
+
+    res.send({ status: 'OK', data: product });
+  } catch (err) {
+    res.status(400).send({ status: 'Error', message: err.message });
+  }
+};
+
+module.exports = {
+  createProduct,
+  deleteProduct,
+  getProducts,
+  getProductsByUser,
+};
